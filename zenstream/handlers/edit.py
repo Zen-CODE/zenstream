3bd8e97f2cddd1ac672343cf26ef325b97347640
Richard Larkin <zenkey.zencode@gmail.com>
2026-08-23 20:33:32 +0200

    :art: Tweak layout

--- zenstream/handlers/edit.py
+++ zenstream/handlers/edit.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
 
 
 class EditFile:
@@ -14,21 +15,27 @@ class EditFile:
         return ""
 
     @staticmethod
-    def _save_text(file_name: str, text_area: str):
+    def _save_text(file_name: str, text_area: str, container: DeltaGenerator):
         """Save the text to the file."""
         try:
             with open(file_name, "w", encoding="utf-8") as f:
                 f.write(text_area)
-            st.success("File saved successfully!")
+            container.success("File saved successfully!")
         except Exception as e:
-            st.warning(f"Unable to save the file...({e})")
+            container.warning(f"Unable to save the file...({e})")
 
     @staticmethod
     def edit_file(file_name: str):
         """Display the file details."""
-
-        text = EditFile._load_text(file_name)
-
+        with st.container():
+            col1, col2 = st.columns([0.25, 0.75])
+            text = EditFile._load_text(file_name)
+            col1.markdown("**Editing file**")
+            col2.write(f"{file_name}")
         text_area = st.text_area("Notes", value=text, height=600)
 
-        st.button("Save", on_click=lambda: EditFile._save_text(file_name, text_area))
+        with st.container():
+            col1, col2 = st.columns([0.25, 0.75])
+            col1.button(
+                "Save", on_click=lambda: EditFile._save_text(file_name, text_area, col2)
+            )
