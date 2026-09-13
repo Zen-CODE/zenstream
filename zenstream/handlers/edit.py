242b3e71cdcbbaa8434fd41ce1b9eb43150b7dee
Richard Larkin <zenkey.zencode@gmail.com>
2026-09-13 03:52:53 +0200

    :sparkles: Extracted shellrun

--- zenstream/handlers/edit.py
+++ zenstream/handlers/edit.py
@@ -48,6 +48,43 @@ class PythonHandler:
                 container.code(result.stderr)
 
 
+class ShellRun:
+    @staticmethod
+    def _run(file_name: str):
+        """Run the shell file."""
+        file_path = Path(file_name).parent
+        return subprocess.run(
+            ["sh", file_name], capture_output=True, text=True, cwd=file_path
+        )
+
+    @staticmethod
+    def add_buttons(
+        container: DeltaGenerator, file_name: str, output_box: DeltaGenerator
+    ):
+        """Add action buttons for the given file."""
+        Styler.add_button(
+            container,
+            "Run",
+            Action.get_icon(file_name),
+            on_click=lambda: ShellRun.run_file(file_name, output_box),
+        )
+
+    @staticmethod
+    def run_file(file_name: str, container: DeltaGenerator) -> None:
+        """Run the specified file"""
+
+        with container.spinner(f"Running {file_name}"):
+            result = ShellRun._run(file_name)
+
+            print(f"Ran : results - {result}")
+            if result.returncode == 0:
+                container.markdown("**✅ Output**")
+                container.code(result.stdout)
+            else:
+                container.subheader("**⚠️ Error**")
+                container.code(result.stderr)
+
+
 class EditFile:
     @staticmethod
     def _load_text(file_name: str) -> str:
@@ -71,26 +108,6 @@ class EditFile:
             container.warning(f"Unable to save the file...({e})")
 
     @staticmethod
-    def _run_file(file_name: str, container: DeltaGenerator) -> None:
-        """Run the specified file"""
-
-        with container.spinner(f"Running {file_name}"):
-            match get_ext(file_name):
-                case "sh":
-                    result = subprocess.run(
-                        ["sh", file_name], capture_output=True, text=True
-                    )
-                case _:
-                    container.warning(f"Unrecognized file type: {file_name}")
-                    return
-            if result.returncode == 0:
-                container.markdown("**✅ Output**")
-                container.code(result.stdout)
-            else:
-                container.subheader("**⚠️ Error**")
-                container.code(result.stderr)
-
-    @staticmethod
     def edit_file(file_name: str):
         """Display the file details."""
         text = EditFile._load_text(file_name)
@@ -102,6 +119,9 @@ class EditFile:
             case "py":
                 col1, col2 = button_box.columns([0.5, 0.5])
                 PythonHandler.add_buttons(col2, file_name, output_box)
+            case "sh":
+                col1, col2 = button_box.columns([0.5, 0.5])
+                ShellRun.add_buttons(col2, file_name, output_box)
             case _:
                 col1 = button_box
 
